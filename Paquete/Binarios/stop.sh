e6caa39b8479bce501fd3c19c5ddd3fca4550556
Facu07 <facuwalter7@gmail.com>
2019-10-01 17:49:49 -0300

    harcodeado el logueo de stop.sh

--- Paquete/Binarios/stop.sh
+++ Paquete/Binarios/stop.sh
@@ -2,16 +2,19 @@
 
 NOMBRE_PROCESO=proc.sh
 PID_PROCESO=`ps -a | grep $NOMBRE_PROCESO | awk '{print $1}'`
-
+DIRACTUAL=$(pwd)
+cd ..
+DIRLOG="$(pwd)/conf/log"
+cd "$DIRACTUAL"
 if [ -z $PID_PROCESO ]
 then
-  echo "El programa no se encuentra ejecutado."
-  $BINDIR./glog.sh "stop" "El programa no se encuentra ejecutado." "ERROR"
+	echo "El programa no se encuentra ejecutado."
+	echo "$(date +%Y-%m-%d_%H:%M:%S) - ${USER} - stop - INFO - El programa no se encuentra ejecutado." >> "$DIRLOG/stop.log"
 else
-  #mandamos senial para terminar el proceso	
-  kill -15 $PID_PROCESO
-  echo "Finalizando el programa con pid: $PID_PROCESO"
-  $BINDIR./glog.sh "stop" "Finalizando el programa con pid: $PID_PROCESO"
+	#mandamos senial para terminar el proceso	
+	kill -15 $PID_PROCESO
+	echo "Finalizando el programa con pid: $PID_PROCESO"
+	echo "$(date +%Y-%m-%d_%H:%M:%S) - ${USER} - stop - INFO - Finalizando el programa con pid: $PID_PROCESO" >> "$DIRLOG/stop.log"
 fi
 
 exit 0
